2be5e76030af3658652e89921d15c864cccfbb2d
sonnaila venkatesham <sonnailavenkatesham95@gmail.com>
2024-04-15 12:07:28 +0530

    added files

--- variables.sh
+++ variables.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 DATE=$(date +%x-%T)
 echo $DATE
-echo "Please enter your ID:"
-read id
-if [ $id -eq 0296 ]
-then
-    echo "Hi Rakesh, Welcome to Devops"
-else
-    echo "Please provide correct name"
-    exit 1
-fi
+echo "Please enter your name:"
+read name
+echo "Hi $name, Welcome to Devops"
+
 
 
 

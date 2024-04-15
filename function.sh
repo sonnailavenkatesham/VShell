13c137b51e2aa18526499e570e1666f120c2c333
sonnaila venkatesham <sonnailavenkatesham95@gmail.com>
2024-04-15 12:24:51 +0530

    added files

---
+++ function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+echo "Hi..there"
+name1=$1
+name2=$2
+
+echo "Hi $name1"
+echo "Hello $name2"
+
+
+
+
+
+# VALIDATE(){
+# if [ $1 -ne 0 ]
+# then  
+#     echo -e "$R $2..FAILED $N"
+#     exit 1
+# else    
+#     echo -e " $2..$G SUCCESS $N"
+# fi
+# }

61c92013769937ca78aefb40431ebe7076d8a745
sonnaila venkatesham <sonnailavenkatesham95@gmail.com>
2024-04-15 12:35:15 +0530

    added files

--- function.sh
+++ function.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2....FAILED $N"
+        exit 1
+    else
+        echo -e "$G $2....SUCCESSFULL $N"
+    fi
+}
 
-echo "Hi..there"
-name1=$1
-name2=$2
-
-echo "Hi $name1"
-echo "Hello $name2"
-
-
-
-
-
-# VALIDATE(){
-# if [ $1 -ne 0 ]
-# then  
-#     echo -e "$R $2..FAILED $N"
-#     exit 1
-# else    
-#     echo -e " $2..$G SUCCESS $N"
-# fi
-# }
+echo "Hello Rakesh"
+# VALIDATE $? "module disable nodejs"

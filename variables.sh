8a812a1a0ed6967af67067bace8af570a03bf5ed
sonnaila venkatesham <sonnailavenkatesham95@gmail.com>
2024-04-15 11:59:31 +0530

    added files

--- variables.sh
+++ variables.sh
@@ -3,6 +3,13 @@ DATE=$(date +%x-%T)
 echo $DATE
 echo "Please enter name:"
 read name
+if [ $name -eq "venkat" ]
+then
+    echo "Welcome to Devops"
+else
+    echo "Please provide correct name"
+    exit1
+fi
 
 
 

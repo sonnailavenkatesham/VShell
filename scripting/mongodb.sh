7e2b182e7748007a96d430e02ebe48b56ab37ff8
sonnaila venkatesham <sonnailavenkatesham95@gmail.com>
2024-04-16 11:25:44 +0530

    added a file

--- scripting/mongodb.sh
+++ scripting/mongodb.sh
@@ -17,11 +17,46 @@ else
 fi
 
 yum install mongodb-org -y
+if [ $? -eq 0 ]
+then
+    echo -e "$G SUCCESS$N"
+else
+    echo -e "$R FAILED $N"
+    exit 1
+fi
 
 systemctl enable mongod
+if [ $? -eq 0 ]
+then
+    echo -e "$G SUCCESS$N"
+else
+    echo -e "$R FAILED $N"
+    exit 1
+fi
 
 systemctl start mongod
+if [ $? -eq 0 ]
+then
+    echo -e "$G SUCCESS$N"
+else
+    echo -e "$R FAILED $N"
+    exit 1
+fi
 
 sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+if [ $? -eq 0 ]
+then
+    echo -e "$G SUCCESS$N"
+else
+    echo -e "$R FAILED $N"
+    exit 1
+fi
 
 systemctl restart mongod
+if [ $? -eq 0 ]
+then
+    echo -e "$G SUCCESS$N"
+else
+    echo -e "$R FAILED $N"
+    exit 1
+fi

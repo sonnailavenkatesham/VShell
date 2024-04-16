e830de7b76df69a9058c7b13a6f9265bfdf8e611
sonnaila venkatesham <sonnailavenkatesham95@gmail.com>
2024-04-16 11:09:20 +0530

    added a file

---
+++ scripting/mongodb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+if [ $? -eq 0 ]
+then
+    echo -e "$G SUCCESS$N"
+else
+    echo -e "$R FAILED $N"
+    exit 1
+fi
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+
+systemctl start mongod
+
+sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+systemctl restart mongod
